fb0c105f27552e9a9d5f42c673223b9a1e8cf4d5
dinusha_dck <dinusha@duosoftware.com>
2015-04-16 10:55:15 +0530

    Channel Status Rest Api Methods Done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var restify = require('restify');
 var stringify = require('stringify');
 var dbHandler = require('./DBBackendHandler.js');
 var redisHandler = require('./RedisHandler.js');
-var messageFormatter = require('./DVP-Common/CommonMessageGenerator.js')
+var messageFormatter = require('./DVP-Common/CommonMessageGenerator/ClientMessageJsonFormatter.js');
 
 var server = restify.createServer({
     name: 'localhost',
@@ -12,6 +12,94 @@ var server = restify.createServer({
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
+
+var AddToChannelArray = function(chanTags, chanList, callback)
+{
+    try
+    {
+        var len = chanTags.length;
+        var count = 0;
+
+        chanTags.forEach(function(tag)
+        {
+            redisHandler.GetFromHash(tag, function(err, hashObj)
+            {
+                if(count < len)
+                {
+                    var channelData =
+                    {
+                        ChannelState: hashObj["Channel-State"],
+                        FreeSwitchName: hashObj["FreeSWITCH-Switchname"],
+                        ChannelName: hashObj["Channel-Name"],
+                        CallDirection: hashObj["Call-Direction"],
+                        CallerDestinationNumber : hashObj["Caller-Destination-Number"],
+                        OtherLegUuid : hashObj["Other-Leg-Unique-ID"],
+                        CallType : hashObj["Call-Type"]
+                    };
+
+                    chanList.push(channelData);
+
+                    count++;
+
+                    if(count >= len)
+                    {
+                        callback(err, chanList);
+                    }
+                }
+                else
+                {
+                    callback(err, chanList);
+                }
+            })
+
+        });
+    }
+    catch(ex)
+    {
+        callback(ex, userList);
+    }
+};
+
+var AddToArray = function(userTags, userList, callback)
+{
+    try
+    {
+        var len = userTags.length;
+        var count = 0;
+
+        userTags.forEach(function(tag)
+        {
+                redisHandler.GetFromHash(tag, function(err, hashObj)
+                {
+                    if(count < len)
+                    {
+                        var user = {
+                            SipUsername: hashObj.username,
+                            RegistrationStatus: hashObj.RegisterState
+                        };
+
+                        userList.push(user);
+
+                        count++;
+
+                        if(count >= len)
+                        {
+                            callback(err, userList);
+                        }
+                    }
+                    else
+                    {
+                        callback(err, userList);
+                    }
+                })
+
+        });
+    }
+    catch(ex)
+    {
+        callback(ex, userList);
+    }
+};
 
 server.get('/DVP/API/:version/MonitorRestAPI/GetSipRegDetailsByCompany/:companyId/:tenantId', function(req, res, next)
 {
@@ -32,27 +120,24 @@ server.get('/DVP/API/:version/MonitorRestAPI/GetSipRegDetailsByCompany/:companyI
                     if(userTags && userTags.length > 0)
                     {
                         //get all user hash sets from redis
-                        userTags.forEach(function(tag)
+                        AddToArray(userTags, userList, function(err, arrRes)
                         {
-                            redisHandler.GetFromHash(tag, function(err, hashObj)
+                            if(err)
                             {
-                                var user = {
-                                    SipUsername : hashObj.SipUsername,
-                                    RegistrationStatus : hashObj.RegStatus
-                                };
+                                var jsonStr = JSON.stringify(userList);
+                                res.end(jsonStr);
+                            }
+                            else
+                            {
+                                var jsonStr = JSON.stringify(arrRes);
+                                res.end(jsonStr);
+                            }
 
-                                userList.push(user);
-                            })
                         })
-
-                        var jsonString = JSON.stringify(userList);
-
-                        res.end(jsonString);
                     }
                     else
                     {
                         var jsonString = JSON.stringify(userList);
-
                         res.end(jsonString);
                     }
                 });
@@ -95,18 +180,28 @@ server.get('/DVP/API/:version/MonitorRestAPI/GetSipRegDetailsByUser/:user/:compa
 
                 redisHandler.GetFromHash(tag, function (err, hashObj)
                 {
-                    var user = {
-                        SipUsername: hashObj.SipUsername,
-                        RegistrationStatus: hashObj.RegStatus,
-                        ExtraData: json.parse(hashObj.Data)
-                    };
+                    if(err)
+                    {
+                        res.end('{}');
+                    }
+                    else
+                    {
+                        var sipUser = {
+                            SipUsername: hashObj.username,
+                            RegistrationStatus: hashObj.RegisterState,
+                            ExtraData: JSON.parse(hashObj.Data)
 
-                    userList.push(user);
-                })
+                        };
 
-                var jsonString = JSON.stringify(user);
+                        var jsonString = JSON.stringify(sipUser);
 
-                res.end(jsonString);
+                        res.end(jsonString);
+                    }
+
+
+                });
+
+
             }
             else
             {
@@ -114,6 +209,119 @@ server.get('/DVP/API/:version/MonitorRestAPI/GetSipRegDetailsByUser/:user/:compa
             }
         });
 
+
+        return next();
+    }
+    catch(ex)
+    {
+        var jsonString = messageFormatter.FormatMessage(ex, "ERROR", false, undefined);
+        res.end(jsonString);
+    }
+
+    return next();
+
+});
+
+server.get('/DVP/API/:version/MonitorRestAPI/GetChannelById/:channelId', function(req, res, next)
+{
+    try
+    {
+        var channelId = req.params.channelId;
+
+
+                //Get Registration Details From Redis
+
+                redisHandler.GetFromHash(channelId, function (err, hashObj)
+                {
+                    if (err)
+                    {
+                        res.end('{}');
+                    }
+                    else
+                    {
+                        var channelData =
+                        {
+                            ChannelState: hashObj["Channel-State"],
+                            FreeSwitchName: hashObj["FreeSWITCH-Switchname"],
+                            ChannelName: hashObj["Channel-Name"],
+                            CallDirection: hashObj["Call-Direction"],
+                            CallerDestinationNumber : hashObj["Caller-Destination-Number"],
+                            OtherLegUuid : hashObj["Other-Leg-Unique-ID"],
+                            CallType : hashObj["Call-Type"]
+                        };
+
+                        var jsonString = JSON.stringify(channelData);
+
+                        res.end(jsonString);
+                    }
+
+
+                });
+
+
+
+
+        return next();
+    }
+    catch(ex)
+    {
+        var jsonString = messageFormatter.FormatMessage(ex, "ERROR", false, undefined);
+        res.end(jsonString);
+    }
+
+    return next();
+
+});
+
+server.get('/DVP/API/:version/MonitorRestAPI/GetChannelsByCompany/:companyId/:tenantId', function(req, res, next)
+{
+    try
+    {
+        var companyId = req.params.companyId;
+        var tenantId = req.params.tenantId;
+
+        var chanList = [];
+
+        dbHandler.GetDomainByCompany(companyId, tenantId, function (err, endUser)
+        {
+            if(endUser && endUser.Domain)
+            {
+                //Get Registration Details From Redis
+                redisHandler.GetFromSet('CHANNEL@' + endUser.Domain, function(err, chanTags)
+                {
+                    if(chanTags && chanTags.length > 0)
+                    {
+                        //get all user hash sets from redis
+                        AddToChannelArray(chanTags, chanList, function(err, arrRes)
+                        {
+                            if(err)
+                            {
+                                var jsonStr = JSON.stringify(chanList);
+                                res.end(jsonStr);
+                            }
+                            else
+                            {
+                                var jsonStr = JSON.stringify(chanList);
+                                res.end(jsonStr);
+                            }
+
+                        })
+                    }
+                    else
+                    {
+                        var jsonString = JSON.stringify(chanList);
+                        res.end(jsonString);
+                    }
+                });
+
+            }
+            else
+            {
+                var jsonString = JSON.stringify(chanList);
+
+                res.end(jsonString);
+            }
+        });
 
         return next();
     }

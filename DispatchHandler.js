bbef97e7290570e434f2cb5e396fecd6ab851860
Pawan Sasanka <pawan@duosoftware.com>
2018-12-05 16:22:29 +0530

    caller number sent to dispatch

--- DispatchHandler.js
+++ DispatchHandler.js
@@ -776,6 +776,11 @@ var CallDispatch = function (tenantId, companyId, bargeMethod, req, res) {
 
                                     var dialoption = format("return_ring_ready=false,origination_uuid={0},origination_caller_id_number={1},DVP_ACTION_CAT={2},DVP_OPERATION_CAT=PRIVATE_USER,companyid={3},tenantid={4},DVP_CALLMONITOR_OTHER_LEG={5}", reqId, channelId, dvpActionCat, companyId, tenantId, channelId);
 
+                                    if(req.query.callernum)
+                                    {
+                                        dialoption = format("return_ring_ready=false,origination_uuid={0},origination_caller_id_number={1},DVP_ACTION_CAT={2},DVP_OPERATION_CAT=PRIVATE_USER,companyid={3},tenantid={4},DVP_CALLMONITOR_OTHER_LEG={5}", reqId, req.query.callernum, dvpActionCat, companyId, tenantId, channelId);
+                                    }
+
                                     logger.debug('[DVP-MonitorRestAPI.CallDispatch] - [%s] - options : %s', reqId, dialoption);
 
                                     if (protocol.toLowerCase() == "user")

c69bae93f83bfa1cebc7b3f0500601bcf46ae308
duobcoder <60178186+duobcoder@users.noreply.github.com>
2020-05-17 12:32:17 +0530

    updated env variables

--- config/default.js
+++ config/default.js
@@ -1,23 +1,23 @@
 module.exports = {
     "DB": {
         "Type":"postgres",
-        "User":"duo",
-        "Password":"DuoS123",
+        "User":"postgres",
+        "Password":"",
         "Port":5432,
-        "Host":"104.236.231.11",
+        "Host":"",
         "Database":"duo"
     },
 
     "Redis":
         {
             "mode":"instance",//instance, cluster, sentinel
-            "ip": "138.197.90.92",
-            "port": 6389,
-            "user": "duo",
-            "password": "DuoS123",
+            "ip": "",
+            "port": 6379,
+            "user": "",
+            "password": "",
             "sentinels":{
-                "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
-                "port":16389,
+                "hosts": "",
+                "port":6379,
                 "name":"redis-cluster"
             }
 
@@ -27,14 +27,14 @@ module.exports = {
     "Security":
         {
 
-            "ip" : "45.55.142.207",
-            "port": 6389,
-            "user": "duo",
-            "password": "DuoS123",
+            "ip" : "",
+            "port": 6379,
+            "user": "",
+            "password": "",
             "mode":"instance",//instance, cluster, sentinel
             "sentinels":{
-                "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
-                "port":16389,
+                "hosts": "",
+                "port":6379,
                 "name":"redis-cluster"
             }
         },
